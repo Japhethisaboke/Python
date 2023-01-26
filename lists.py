80ecc5ae8c0606ff4e3dd4fa9d873387db6ef6b1
Japhethisaboke <japhethjapho7@gmai.com>
2023-01-26 17:33:27 +0300

    Updated codes

---
+++ lists.py
@@ -0,0 +1,9 @@
+#Define a list of Names
+names = ["Harry", "Japheth", "Joy"]
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
+
---
+++ sequences.py
@@ -0,0 +1,4 @@
+coordinateX = 10.2
+coordinateY = 20.5
+
+coordinate = (10.0,20.0)
